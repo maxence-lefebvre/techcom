81adf7ffc08c89466ca7c24cacd37e18a936aab4
mlfbvr <maxence.lfbvr@gmail.com>
2021-12-01 00:26:44 +0100

    chore: init

---
+++ apps/portal/components/test/test.tsx
@@ -0,0 +1,14 @@
+import './test.module.scss';
+
+/* eslint-disable-next-line */
+export interface TestProps {}
+
+export function Test(props: TestProps) {
+  return (
+    <div>
+      <h1>Welcome to Test!</h1>
+    </div>
+  );
+}
+
+export default Test;
